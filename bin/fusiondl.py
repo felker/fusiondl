a851628b450c59bc720fc18dc87452772abe01c8
Kyle Gerard Felker <felker@anl.gov>
2021-03-30 10:35:40 -0500

    Add custom targets and losses

--- bin/fusiondl.py
+++ bin/fusiondl.py
@@ -7,18 +7,22 @@ import numpy as np
 import argparse
 import errno
 # import hydra
-
+# TODO(KGF): setup logging. Hydra will setup logging for project, which would break my
+# setup
 
 exec_dir = os.path.dirname(os.path.abspath(__file__))
 proj_dir = os.path.dirname(exec_dir)
 sys.path.insert(0, proj_dir)
 
+# TODO(KGF): global_vars stores MPI communicator, conf dictionary, and fns for clean
+# parallel stdout. Probably better alternative is to use large "class exec():" with
+# self.args, etc. and explicitly pass around the conf to make_trainer() etc.
 import src.global_vars as g
 # only places CosmicTagger uses MPI outside Horovod is for logging in exec.py
 # and torch/distributed_trainer.py iff DDP
 g.init_MPI()
 
-from src.config.conf_parser import parameters
+from src.config.conf_parser import read_parameters
 
 # set PRNG seed, unique for each worker, based on MPI task index for
 # reproducible shuffling in guranteed_preprocessed() and training steps
@@ -26,8 +30,6 @@ np.random.seed(g.task_index)
 random.seed(g.task_index)
 
 
-
-# TODO(KGF): replace this workaround for the "from plasma.conf import conf"
 def is_valid_file(parser, arg):
     if not (os.path.exists(arg) and os.path.isfile(arg)):
         parser.error("The file %s does not exist!" % arg)
@@ -65,50 +67,87 @@ def make_parser():
 ## def init_mpi():
 
 
-def normalize(conf):
-    if conf['data']['normalizer'] == 'minmax':
-        from src.preprocessor.normalize import MinMaxNormalizer as Normalizer
-    elif conf['data']['normalizer'] == 'meanvar':
-        from src.preprocessor.normalize import MeanVarNormalizer as Normalizer
-    elif conf['data']['normalizer'] == 'var':
-        # performs !much better than minmaxnormalizer
-        from src.preprocessor.normalize import VarNormalizer as Normalizer
-    elif conf['data']['normalizer'] == 'averagevar':
-        # performs !much better than minmaxnormalizer
-        from src.preprocessor.normalize import (
-            AveragingVarNormalizer as Normalizer
-        )
-    else:
-        print('unkown normalizer. exiting')
-        exit(1)
-    normalizer = Normalizer(conf)
-    if g.task_index == 0:
-        # make sure preprocessing has been run, and results are saved to files
-        # if not, only master MPI rank spawns thread pool to perform preprocessing
-        (shot_list_train, shot_list_validate,
-         shot_list_test) = guarantee_preprocessed(conf)
-        # similarly, train normalizer (if necessary) w/ master MPI rank only
-        normalizer.train()  # verbose=False only suppresses if purely loading
+# def normalize(conf):
+#     if conf['data']['normalizer'] == 'minmax':
+#         from src.preprocessor.normalize import MinMaxNormalizer as Normalizer
+#     elif conf['data']['normalizer'] == 'meanvar':
+#         from src.preprocessor.normalize import MeanVarNormalizer as Normalizer
+#     elif conf['data']['normalizer'] == 'var':
+#         # performs !much better than minmaxnormalizer
+#         from src.preprocessor.normalize import VarNormalizer as Normalizer
+#     elif conf['data']['normalizer'] == 'averagevar':
+#         # performs !much better than minmaxnormalizer
+#         from src.preprocessor.normalize import (
+#             AveragingVarNormalizer as Normalizer
+#         )
+#     else:
+#         print('unkown normalizer. exiting')
+#         exit(1)
+#     normalizer = Normalizer(conf)
+#     if g.task_index == 0:
+#         # make sure preprocessing has been run, and results are saved to files
+#         # if not, only master MPI rank spawns thread pool to perform preprocessing
+#         (shot_list_train, shot_list_validate,
+#          shot_list_test) = guarantee_preprocessed(conf)
+#         # similarly, train normalizer (if necessary) w/ master MPI rank only
+#         normalizer.train()  # verbose=False only suppresses if purely loading
+#         g.comm.Barrier()
+#         g.print_unique("begin preprocessor+normalization (all MPI ranks)...")
+#         # second call has ALL MPI ranks load preprocessed shots from .npz files
+#         (shot_list_train, shot_list_validate,
+#          shot_list_test) = guarantee_preprocessed(conf, verbose=True)
+#         # second call to normalizer training
+#         normalizer.conf['data']['recompute_normalization'] = False
+#         normalizer.train(verbose=True)
+#         # KGF: may want to set it back...
+#         # normalizer.conf['data']['recompute_normalization'] = conf['data']['recompute_normalization']   # noqa
+#         loader = Loader(conf, normalizer)
+#         g.print_unique("...done")
+
+#         # TODO(KGF): both preprocess.py and normalize.py are littered with print()
+#         # calls that should probably be replaced with print_unique() when they are not
+#         # purely loading previously-computed quantities from file
+#         # (or we can continue to ensure that they are only ever executed by 1 rank)
+
+
+
+
+
+def make_trainer(conf):
+    if conf['framework'] == 'tensorflow' or conf['framework'] == 'tf':
+        import tensorflow as tf
+
+        # Prevent Keras TF backend deprecation messages from mpi_train() from
+        # appearing jumbled with stdout, stderr msgs from above steps
         g.comm.Barrier()
-        g.print_unique("begin preprocessor+normalization (all MPI ranks)...")
-        # second call has ALL MPI ranks load preprocessed shots from .npz files
-        (shot_list_train, shot_list_validate,
-         shot_list_test) = guarantee_preprocessed(conf, verbose=True)
-        # second call to normalizer training
-        normalizer.conf['data']['recompute_normalization'] = False
-        normalizer.train(verbose=True)
-        # KGF: may want to set it back...
-        # normalizer.conf['data']['recompute_normalization'] = conf['data']['recompute_normalization']   # noqa
-        loader = Loader(conf, normalizer)
-        g.print_unique("...done")
+        g.flush_all_inorder()
 
-        # TODO(KGF): both preprocess.py and normalize.py are littered with print()
-        # calls that should probably be replaced with print_unique() when they are not
-        # purely loading previously-computed quantities from file
-        # (or we can continue to ensure that they are only ever executed by 1 rank)
+        if tf.__version__.startswith("2"):
+            if conf['distributed']:
+                from src.trainers.tensorflow2 import distributed_trainer
+                trainer = distributed_trainer.distributed_trainer(conf)
+            else:
+                from src.trainers.tensorflow2 import trainer
+                trainer = trainer.tf_trainer(conf)
+        else:
+            if conf['distributed']:
+                from src.trainers.tensorflow1 import distributed_trainer
+                trainer = distributed_trainer.distributed_trainer(conf)
+            else:
+                from src.trainers.tensorflow1 import trainer
+                trainer = trainer.tf_trainer(conf)
+    elif conf['framework'] == "torch":
+        if conf['distributed']:
+            from src.trainers.torch import distributed_trainer
+            trainer = distributed_trainer.distributed_trainer(conf)
+        else:
+            from src.trainers.torch import trainer
+            trainer = trainer.torch_trainer(conf)
+
+    return trainer
 
 
-def train():
+def train(conf, trainer):
 
 
 def main():
@@ -118,24 +157,21 @@ def main():
     g.conf_file = args.conf_file
     if g.conf_file is not None:
         g.print_unique(f"Loading configuration from {g.conf_file}")
-        conf = parameters(g.conf_file)
+        conf = read_parameters(g.conf_file)
     elif os.path.exists('./conf.yaml'):
         g.print_unique(f"Loading configuration from {./conf.yaml}")
-        conf = parameters('./conf.yaml')
+        conf = read_parameters('./conf.yaml')
     else:
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
                                 'conf.yaml')
+    # add CLI switches to conf
+    conf['distributed'] = args.distributed
 
     g.conf = conf
 
     # normalize(g.conf)
-
-    # Prevent Keras TF backend deprecation messages from mpi_train() from
-    # appearing jumbled with stdout, stderr msgs from above steps
-    g.comm.Barrier()
-    g.flush_all_inorder()
-
-    train(g.conf)
+    trainer = make_trainer(g.conf)
+    train(g.conf, trainer)
 
 
 if __name__ == '__main__':
---
+++ src/models/tensorflow/targets.py
@@ -0,0 +1,188 @@
+import numpy as np
+import abc
+
+from src.utils.evaluation import (
+    mse_np, binary_crossentropy_np, hinge_np,
+    )
+import tensorflow as tf  # noqa
+from tensorflow.keras.losses import hinge
+
+# remapper() method, used only in normalize.py, implicitly knows the
+# transformation applied to Shot.ttd within Shot.convert_to_ttd()
+
+
+# Requirement: larger value must mean disruption more likely.
+class Target(object):
+    activation = 'linear'
+    loss = 'mse'
+
+    @abc.abstractmethod
+    def loss_np(y_true, y_pred):
+        from plasma.conf import conf
+        return conf['model']['loss_scale_factor']*mse_np(y_true, y_pred)
+
+    @abc.abstractmethod
+    def remapper(ttd, T_warning):
+        # TODO(KGF): base class directly uses ttd=log(TTD+dt/10) quantity
+        return -ttd
+
+
+class BinaryTarget(Target):
+    activation = 'sigmoid'
+    loss = 'binary_crossentropy'
+
+    @staticmethod
+    def loss_np(y_true, y_pred):
+        from plasma.conf import conf
+        return (conf['model']['loss_scale_factor']
+                * binary_crossentropy_np(y_true, y_pred))
+
+    @staticmethod
+    def remapper(ttd, T_warning, as_array_of_shots=True):
+        # TODO(KGF): see below comment in HingeTarget.remapper()
+        binary_ttd = 0*ttd
+        mask = ttd < np.log10(T_warning)
+        binary_ttd[mask] = 1.0
+        binary_ttd[~mask] = 0.0
+        return binary_ttd
+
+
+class LogTTDTarget(Target):
+    activation = 'linear'
+    loss = 'mse'
+
+    @staticmethod
+    def loss_np(y_true, y_pred):
+        from plasma.conf import conf
+        return conf['model']['loss_scale_factor']*mse_np(y_true, y_pred)
+
+    @staticmethod
+    def remapper(ttd, T_warning):
+        mask = ttd < np.log10(T_warning)
+        ttd[~mask] = np.log10(T_warning)
+        return -ttd
+
+
+class TTDInvTarget(Target):
+    activation = 'linear'
+    loss = 'mse'
+
+    @staticmethod
+    def loss_np(y_true, y_pred):
+        return mse_np(y_true, y_pred)
+
+    @staticmethod
+    def remapper(ttd, T_warning):
+        eps = 1e-4  # hardcoded "avoid division by zero"
+        ttd = 10**(ttd)  # see below comment about undoing log transformation
+        mask = ttd < T_warning
+        ttd[~mask] = T_warning
+        ttd = (1.0)/(ttd + eps)
+        return ttd
+
+
+class TTDLinearTarget(Target):
+    activation = 'linear'
+    loss = 'mse'
+
+    @staticmethod
+    def loss_np(y_true, y_pred):
+        from plasma.conf import conf
+        return conf['model']['loss_scale_factor']*mse_np(y_true, y_pred)
+
+    @staticmethod
+    def remapper(ttd, T_warning):
+        # TODO(KGF): this next line "undoes" the log-transform in shots.py
+        # Shot.convert_to_ttd() (except for small offset of +dt/10)
+        ttd = 10**(ttd)
+        mask = ttd < T_warning
+        ttd[~mask] = 0  # T_warning
+        ttd[mask] = T_warning - ttd[mask]  # T_warning
+        return ttd
+
+
+# implements a "maximum" driven loss function. Only the maximal value in the
+# time sequence is punished. Also implements class weighting
+class MaxHingeTarget(Target):
+    activation = 'linear'
+    loss = 'hinge'
+    fac = 1.0
+
+    @staticmethod
+    def loss(y_true, y_pred):
+        # TODO(KGF): this function is unused and unique to this class
+        from plasma.conf import conf
+        fac = MaxHingeTarget.fac
+        # overall_fac =
+        # np.prod(np.array(K.shape(y_pred)[1:]).astype(np.float32))
+        overall_fac = K.prod(K.cast(K.shape(y_pred)[1:], K.floatx()))
+        max_val = K.max(y_pred, axis=-2)  # temporal axis!
+        max_val1 = K.repeat(max_val, K.shape(y_pred)[-2])
+        mask = K.cast(K.equal(max_val1, y_pred), K.floatx())
+        y_pred1 = mask * y_pred + (1-mask) * y_true
+        weight_mask = K.mean(y_true, axis=-1)
+        weight_mask = K.cast(K.greater(weight_mask, 0.0),
+                             K.floatx())  # positive label!
+        weight_mask = fac*weight_mask + (1 - weight_mask)
+        # return weight_mask*squared_hinge(y_true, y_pred1)
+
+        # KGF: this is the only place where tensorflow.keras.losses.hinge()
+        # is used in this file
+        return conf['model']['loss_scale_factor'] * \
+            overall_fac*weight_mask*hinge(y_true, y_pred1)
+
+    @staticmethod
+    def loss_np(y_true, y_pred):
+        from plasma.conf import conf
+        # TODO(KGF): fac = positive_example_penalty is only used in this class,
+        # only in above (unused) loss() fn, which only this class has, and is
+        # never called. 2 lines related to fac commented-out in this fn.
+        #
+        # fac = MaxHingeTarget.fac
+        overall_fac = np.prod(np.array(y_pred.shape).astype(np.float32))
+        max_val = np.max(y_pred, axis=-2)  # temporal axis!
+        max_val = np.reshape(
+            max_val, max_val.shape[:-1] + (1,) + (max_val.shape[-1],))
+        max_val = np.tile(max_val, (1, y_pred.shape[-2], 1))
+        mask = np.equal(max_val, y_pred)
+        mask = mask.astype(np.float32)
+        y_pred = mask * y_pred + (1-mask) * y_true
+        # positive label! weight_mask = fac*weight_mask + (1 - weight_mask):
+        weight_mask = np.greater(y_true, 0.0).astype(np.float32)
+        # return np.mean(
+        #  weight_mask*np.square(np.maximum(1. - y_true * y_pred, 0.)))
+        # , axis=-1)
+        # only during training, here we want to completely sum up over all
+        # instances
+        return (conf['model']['loss_scale_factor']
+                * np.mean(overall_fac * weight_mask
+                          * np.maximum(1. - y_true * y_pred, 0.)))
+
+    @staticmethod
+    def remapper(ttd, T_warning, as_array_of_shots=True):
+        # TODO(KGF): see below comment in HingeTarget.remapper()
+        binary_ttd = 0*ttd
+        mask = ttd < np.log10(T_warning)
+        binary_ttd[mask] = 1.0
+        binary_ttd[~mask] = -1.0
+        return binary_ttd
+
+
+class HingeTarget(Target):
+    activation = 'linear'
+    loss = 'hinge'
+
+    @staticmethod
+    def loss_np(y_true, y_pred):
+        from plasma.conf import conf
+        return conf['model']['loss_scale_factor']*hinge_np(y_true, y_pred)
+        # return squared_hinge_np(y_true, y_pred)
+
+    @staticmethod
+    def remapper(ttd, T_warning, as_array_of_shots=True):
+        # TODO(KGF): zeros the ttd=log(TTD+dt/10) (just reuses shape)
+        binary_ttd = 0*ttd
+        mask = ttd < np.log10(T_warning)
+        binary_ttd[mask] = 1.0
+        binary_ttd[~mask] = -1.0
+        return binary_ttd
